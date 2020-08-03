ea888792adf5a2c24085b8d3d334f442655d6836
Irakli Safareli <i.safareli@gmail.com>
2020-08-04 00:33:26 +0400

    initial version

---
+++ src/App.tsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+import Form from "@rjsf/core";
+import ReactMarkdown from "react-markdown";
+
+import { solve, Vote } from "./election";
+
+const schema = {
+  type: "array",
+  title: "Election simulator",
+  description: "Fill simulation voters and there detauls",
+  minItems: 1,
+  items: {
+    title: "Voter",
+    description: "Fill voter details here",
+    type: "object",
+    required: ["voter", "weight", "order"],
+    properties: {
+      voter: {
+        type: "string",
+        title: "Name",
+        default: "Alisa",
+      },
+      weight: {
+        type: "number",
+        title: "Weight",
+        default: 100,
+      },
+      order: {
+        type: "array",
+        title: "Candidates order",
+        minItems: 1,
+        items: {
+          default: ["A"],
+          type: "array",
+          minItems: 1,
+          items: {
+            type: "string",
+            default: "A",
+          },
+        },
+      },
+    },
+  },
+};
+const uiSchema = {
+  classNames: "VoterArray",
+  "ui:options": {
+    orderable: false,
+  },
+  items: {
+    classNames: "Voters",
+    order: {
+      items: {
+        "ui:options": {
+          orderable: false,
+        },
+      },
+    },
+  },
+};
+
+function App() {
+  const [result, setResult] = useState("");
+  const [formData, setFormData] = useState([]);
+  useEffect(() => {
+    const jsonStr = localStorage.getItem("electionInput");
+    if (jsonStr == null) return;
+    setFormData(JSON.parse(jsonStr));
+  }, []);
+  return (
+    <div className="App">
+      <Form
+        schema={schema as any}
+        uiSchema={uiSchema}
+        formData={formData}
+        onSubmit={(res) => {
+          setFormData(res.formData);
+          localStorage.setItem("electionInput", JSON.stringify(res.formData));
+          console.log(res.formData);
+          setResult(
+            Array.from(solve((res.formData as any) as Array<Vote>)).join("\n")
+          );
+        }}
+      />
+      <ReactMarkdown source={result} />
+    </div>
+  );
+}
+export default App;
+
+// solve([
+//   {
+//     voter: "Alisa",
+//     order: [
+//       //
+//       // ["I"],
+//       ["A", "B"],
+//       ["A", "B"],
+//       ["C"],
+//     ],
+//     weight: 100,
+//   },
+//   {
+//     voter: "Bob",
+//     order: [
+//       //
+//       // ["I"],
+//       ["A"],
+//       ["B", "C"],
+//       ["Z"],
+//     ],
+//     weight: 10,
+//   },
+//   {
+//     voter: "John",
+//     order: [
+//       //
+//       // ["I"],
+//       ["C"],
+//       ["B", "K"],
+//       ["A", "Y"],
+//     ],
+//     weight: 70,
+//   },
+// ]);
---
+++ src/election.ts
@@ -0,0 +1,272 @@
+function flatMap<T, U>(
+  array: T[],
+  callbackfn: (value: T, index: number, array: T[]) => U[]
+): U[] {
+  return Array.prototype.concat(...array.map(callbackfn));
+}
+export type Voter = string;
+export type Candidate = string;
+export type PositionProposal = Array<Candidate>;
+export type Weight = number; // integer
+export type Vote = { order: Array<PositionProposal> } & Supporter;
+export type VoteNE = {
+  orderHead: PositionProposal;
+  orderTail: Array<PositionProposal>;
+} & Supporter;
+
+export type Supporter = { voter: Voter; weight: Weight };
+export type Supporters = Array<Supporter>;
+export type CandidateWeighted = {
+  candidate: Candidate;
+  weight: Weight;
+  supporters: Supporters;
+};
+export type Result = Array<CandidateWeighted>;
+
+const removeWinner = (
+  winnerCandidate: Candidate,
+  order: Array<PositionProposal>
+): Array<PositionProposal> => {
+  return order
+    .map((pos) => pos.filter((candidate) => candidate !== winnerCandidate))
+    .filter((x) => x.length > 0);
+};
+const sortByWeight = (votes: Array<VoteNE>): Result => {
+  const candidateWeights: Map<
+    Candidate,
+    {
+      weight: Weight;
+      supporters: Array<{ voter: Voter; weight: Weight }>;
+    }
+  > = new Map();
+  votes.forEach((vote: VoteNE) => {
+    vote.orderHead.forEach((candidate) => {
+      const current = candidateWeights.get(candidate) || {
+        weight: 0,
+        supporters: [],
+      };
+      candidateWeights.set(candidate, {
+        weight: current.weight + vote.weight,
+        supporters: current.supporters.concat({
+          voter: vote.voter,
+          weight: vote.weight,
+        }),
+      });
+    });
+  });
+  const candidatesSorted = Array.from(candidateWeights.entries()).sort(
+    (a, b) => {
+      return b[1].weight - a[1].weight;
+    }
+  );
+  return candidatesSorted.map(([candidate, { weight, supporters }]) => ({
+    candidate,
+    weight,
+    supporters,
+  }));
+};
+
+export type RoundRes =
+  | {
+      type: "FULL_CONSENSUS";
+      winner: CandidateWeighted;
+    }
+  | {
+      type: "NEW_WINNER";
+      winner: CandidateWeighted;
+      winnerOptimized: CandidateWeighted;
+      winningWeightMultiplier: number;
+      // nextVotes: Array<Vote>;
+    }
+  | {
+      type: "SKIP";
+    }
+  | {
+      type: "COLLISION";
+      candidates: Array<CandidateWeighted>;
+    };
+
+function* solveOneRound(
+  votes: Array<VoteNE>
+): Generator<string, RoundRes, unknown> {
+  const candidatesSorted = sortByWeight(votes);
+  yield "# Candidates Sorted: \n\n" +
+    renderResult(candidatesSorted) +
+    "\n\n---";
+
+  if (candidatesSorted.length === 0) {
+    // NOTE it means that in this round everyone had empty candidate
+    // set, probably that should be restricted in UI
+    return { type: "SKIP" } as RoundRes;
+  } else if (candidatesSorted.length === 1) {
+    // NOTE it means that in this round everyone had one candidate
+    // only and because of this no weight will be spend
+    const winner = candidatesSorted[0];
+    return {
+      type: "FULL_CONSENSUS",
+      winner,
+    } as RoundRes;
+  } else {
+    if (candidatesSorted[0].weight === candidatesSorted[1].weight) {
+      return {
+        type: "COLLISION",
+        candidates: candidatesSorted,
+      } as RoundRes;
+    }
+    const winner = candidatesSorted[0];
+
+    // NOTE mainOpponents wouldn't return empty array as here
+    // candidatesSorted.length is 2 or more so the main opponent exists
+    const opponents = sortByWeight(
+      votes.filter(
+        (vote: VoteNE) => vote.orderHead.indexOf(winner.candidate) === -1
+      )
+    );
+    // TODO combine this and first case together
+    if (opponents.length === 0) {
+      return {
+        type: "FULL_CONSENSUS",
+        winner,
+      } as RoundRes;
+    }
+    console.log(opponents);
+    yield "# Opponents Sorted: \n\n" + renderResult(opponents) + "\n\n---";
+    const [mainOpponent] = opponents;
+
+    const minimumWinningWeight = mainOpponent.weight + 1;
+    const weightSpent = minimumWinningWeight / winner.weight;
+    const winningWeightMultiplier = 1 - weightSpent;
+
+    return {
+      type: "NEW_WINNER",
+      winner,
+      winningWeightMultiplier,
+      winnerOptimized: {
+        candidate: winner.candidate,
+        // TODO fix rounding issues
+        supporters: winner.supporters.map((x) => ({
+          voter: x.voter,
+          weight: x.weight - x.weight * winningWeightMultiplier,
+        })),
+        weight: minimumWinningWeight,
+      },
+    } as RoundRes;
+  }
+}
+
+// Array<Vote>, Result]
+export function* solve(votes: Array<Vote>): Generator<string, void, unknown> {
+  const totalResult: Result = [];
+  let nextVotes: Array<Vote> = votes;
+  const satisfiedVotes: Array<Vote> = [];
+  do {
+    const votesNE = flatMap(nextVotes, (vote) => {
+      if (vote.order.length === 0) {
+        satisfiedVotes.push(vote);
+        return [];
+      }
+      const [orderHead, ...orderTail] = vote.order;
+      return [{ orderHead, orderTail, voter: vote.voter, weight: vote.weight }];
+    });
+    if (votesNE.length === 0) {
+      break;
+    }
+    yield "# ROUND STARTED\n\n" + renderVotes(nextVotes) + "\n\n---";
+    const result = yield* solveOneRound(votesNE);
+    switch (result.type) {
+      case "FULL_CONSENSUS":
+        yield "# FULL_CONSENSUS:\n\n" +
+          renderCandidateWeighted(result.winner) +
+          "\n\n---";
+        nextVotes = votesNE.map((vote) => {
+          return {
+            order: removeWinner(result.winner.candidate, vote.orderTail),
+            voter: vote.voter,
+            weight: vote.weight,
+          };
+        });
+        totalResult.push(result.winner);
+        break;
+      case "SKIP":
+        yield "# SKIP\n\n---";
+        nextVotes = votesNE.map((vote) => {
+          return {
+            order: vote.orderTail,
+            voter: vote.voter,
+            weight: vote.weight,
+          };
+        });
+        break;
+      case "NEW_WINNER":
+        yield "# WINNER:\n\n" +
+          renderCandidateWeighted(
+            result.winnerOptimized,
+            ` ~ ${((1 - result.winningWeightMultiplier) * 100).toFixed(
+              2
+            )}% of ${result.winner.weight.toFixed(2)}`
+          ) +
+          "\n\n---";
+        nextVotes = votesNE.map((vote: VoteNE) => {
+          const didVoteForWinner =
+            vote.orderHead.indexOf(result.winner.candidate) !== -1;
+          // TODO fix rounding issues probably by `x - 1`
+          return {
+            voter: vote.voter,
+            weight: didVoteForWinner
+              ? vote.weight * result.winningWeightMultiplier
+              : vote.weight,
+            order: removeWinner(
+              result.winner.candidate,
+              didVoteForWinner
+                ? vote.orderTail
+                : [vote.orderHead, ...vote.orderTail]
+            ),
+          };
+        });
+        totalResult.push(result.winnerOptimized);
+        break;
+      case "COLLISION":
+        yield "# COLLISION:\n\n" + renderResult(result.candidates) + "\n\n---";
+        yield "Accumulated Result:\n\n" + renderResult(totalResult) + "\n---";
+        yield "Satisfied voters:\n\n" + renderVotes(satisfiedVotes);
+        yield "Remaining votes:\n\n" +
+          renderVotes(
+            votesNE.map((vote) => {
+              return {
+                order: [vote.orderHead, ...vote.orderTail],
+                voter: vote.voter,
+                weight: vote.weight,
+              };
+            })
+          );
+        return;
+    }
+  } while (true);
+  yield "# DONE!";
+  yield "Result:\n\n" + renderResult(totalResult) + "\n---";
+  yield "Satisfied weight:\n\n" + renderVotes(satisfiedVotes);
+  // return [nextVotes, totalResult] as [Array<Vote>, Result];
+}
+
+const renderResult = (result: Result) =>
+  result.map((x) => "- " + renderCandidateWeighted(x)).join("\n");
+const renderCandidateWeighted = (
+  candidateWeighted: CandidateWeighted,
+  extra?: string
+) =>
+  `${candidateWeighted.candidate} (${candidateWeighted.weight.toFixed(2)}${
+    extra || ""
+  }):` +
+  candidateWeighted.supporters
+    .map(
+      (supporter: Supporter) =>
+        `\n  - ${supporter.voter} (${supporter.weight.toFixed(2)})`
+    )
+    .join("");
+
+const renderVotes = (votes: Array<Vote>) =>
+  votes.map((x) => "- " + renderVoter(x)).join("\n");
+
+const renderVoter = (vote: Vote) =>
+  `${vote.voter} (${vote.weight.toFixed(2)}):` +
+  vote.order.map((x) => "\n  - " + x.join(",")).join("");
